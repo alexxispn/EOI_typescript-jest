379b8f4b00842159a41997a232715cae6a96f930
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-09-11 13:31:09 +0100

    test(math-operator): add throwIfNan function

--- src/throwIfNaN.ts
+++ src/throwIfNaN.ts
@@ -1,5 +1,5 @@
 import {NAN_ERROR} from "./constants";
 
-export const throwIfNan = (a: number, b: number): void => {
+export const throwIfNaN = (a: number, b: number): void => {
     if (isNaN(a) || isNaN(b)) throw new Error(NAN_ERROR)
 }
