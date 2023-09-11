3c9ba58fd5eb1689e2ac655ba5211b26155f8ef7
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-09-11 14:29:39 +0100

    feat(math-operator): extract error-handlers to a helper folder

--- src/helpers/throwIfNaN.ts
+++ src/helpers/throwIfNaN.ts
@@ -1,5 +1,5 @@
-import {NAN_ERROR} from "./constants";
+import {ERROR_NAN} from "../constants";
 
 export const throwIfNaN = (a: number, b: number): void => {
-    if (isNaN(a) || isNaN(b)) throw new Error(NAN_ERROR)
+    if (isNaN(a) || isNaN(b)) throw new Error(ERROR_NAN)
 }
--- src/helpers/throwIfNumberIsTooBigOrTooSmall.ts
+++ src/helpers/throwIfNumberIsTooBigOrTooSmall.ts
@@ -1,7 +1,7 @@
-import {RESULT_IS_TOO_BIG_OR_TOO_SMALL} from "../constants";
+import {ERROR_NUMBER_IS_TOO_BIG_OR_TOO_SMALL} from "../constants";
 
-export const throwIfResultIsTooBigOrTooSmall = (result: number) => {
-    if ((result > Number.MAX_SAFE_INTEGER) || (result < Number.MIN_SAFE_INTEGER)) {
-        throw new Error(RESULT_IS_TOO_BIG_OR_TOO_SMALL);
+export const throwIfNumberIsTooBigOrTooSmall = (value: number) => {
+    if ((value > Number.MAX_SAFE_INTEGER) || (value < Number.MIN_SAFE_INTEGER)) {
+        throw new Error(ERROR_NUMBER_IS_TOO_BIG_OR_TOO_SMALL);
     }
 }
