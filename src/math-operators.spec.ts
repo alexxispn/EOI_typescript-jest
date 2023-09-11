d99726df7e9b1fd6d97100a1c5b5090ade14929a
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-09-11 13:33:07 +0100

    test(math-operator): add mocks and refactor tests

--- src/math-operators.spec.ts
+++ src/math-operators.spec.ts
@@ -1,43 +1,99 @@
-import {div, multiply, NAN_ERROR, sub, sum} from './math-operators'
+import {div, multiply, sub, sum} from "./math-operators"
+import {NAN_ERROR, RESULT_IS_TOO_BIG_OR_TOO_SMALL} from "./constants";
+import {throwIfNaN} from "./throwIfNaN";
+import {throwIfResultIsTooBigOrTooSmall} from "./throwIfResultIsTooBigOrTooSmall";
 
-describe('Math Operators', () => {
-    describe('sum', () => {
-        it('should add two positive numbers', () => {
-            expect(sum(1, 2)).toBe(3)
+const mockThrowIfNaN = throwIfNaN as jest.MockedFunction<typeof throwIfNaN>
+const mockThrowIfResultIsTooBigOrTooSmall = throwIfResultIsTooBigOrTooSmall as jest.MockedFunction<typeof throwIfResultIsTooBigOrTooSmall>
+
+jest.mock('./throwIfNaN', () => ({
+    throwIfNaN: jest.fn()
+}))
+
+jest.mock('./throwIfResultIsTooBigOrTooSmall', () => ({
+    throwIfResultIsTooBigOrTooSmall: jest.fn()
+}))
+
+beforeEach(() => {
+    mockThrowIfNaN.mockReset()
+    mockThrowIfResultIsTooBigOrTooSmall.mockReset()
+})
+
+describe('sum', () => {
+    it('should return a NaN_ERROR if any of the parameters is NaN', () => {
+        mockThrowIfNaN.mockImplementation(() => {
+            throw new Error(NAN_ERROR)
         })
 
-        it('should return an error if one of the numbers is not a number', () => {
-            expect(() => sum(1, NaN)).toThrowError(NAN_ERROR)
-        })
-
-        it('should sum small decimal numbers', () => {
-            expect(sum(0.1, 0.2)).toBe(0.3)
-        })
-
-        it('should sum big numbers', () => {
-            expect(sum(12345678912345678, 100)).toBe(12345678912345778)
-        })
-
-        it('should sum big numbers with decimals', () => {
-            expect(sum(12345678912345678.1, 100.2)).toBe(12345678912345778.3)
-        })
+        expect(() => sum(NaN, NaN)).toThrowError(NAN_ERROR)
     })
-    describe('sub', () => {
-        it('should subtract two positive numbers', () => {
-            expect(sub(1, 2)).toBe(-1)
+
+    it('should return a RESULT_IS_TOO_BIG_OR_TOO_SMALL if the result is too big or too small', () => {
+        mockThrowIfResultIsTooBigOrTooSmall.mockImplementation(() => {
+            throw new Error(RESULT_IS_TOO_BIG_OR_TOO_SMALL)
         })
-        it('should subtract decimal numbers', () => {
-            expect(sub(1.5, 2.5)).toBe(-1)
-        })
+        const bigNumber = 9007199254740992
+        const smallNumber = -9007199254740992
+
+        expect(() => sum(bigNumber, 100)).toThrowError(RESULT_IS_TOO_BIG_OR_TOO_SMALL)
+        expect(() => sum(smallNumber, -100)).toThrowError(RESULT_IS_TOO_BIG_OR_TOO_SMALL)
     })
-    describe('multiply', () => {
-        it('should multiply two positive numbers', () => {
-            expect(multiply(1, 2)).toBe(2)
-        })
+
+    it('should sum two integers', () => {
+        expect(sum(1, 1)).toBe(2)
     })
-    describe('div', () => {
-        it('should divide two positive numbers', () => {
-            expect(div(1, 2)).toBe(0.5)
+
+    it('should sum two small floats', () => {
+        expect(sum(0.1, 0.2)).toStrictEqual(0.3)
+    })
+})
+
+describe('sub', () => {
+    it('should return a NAN_ERROR if any of the parameters is NaN', () => {
+        mockThrowIfNaN.mockImplementation(() => {
+            throw new Error(NAN_ERROR)
         })
+
+        expect(() => sub(NaN, NaN)).toThrowError(NAN_ERROR)
+    })
+
+    it('should sub two integers', () => {
+        expect(sub(1, 1)).toStrictEqual(0)
+    })
+})
+
+describe('multiply', () => {
+    it('should return a NAN_ERROR if any of the parameters is NaN', () => {
+        mockThrowIfNaN.mockImplementation(() => {
+            throw new Error(NAN_ERROR)
+        })
+
+        expect(() => multiply(NaN, NaN)).toThrowError(NAN_ERROR)
+    })
+
+    it('should multiply two integers', () => {
+        expect(multiply(1, 1)).toStrictEqual(1)
+    })
+})
+
+describe('div', () => {
+    it('should return a NAN_ERROR if any of the parameters is NaN', () => {
+        mockThrowIfNaN.mockImplementation(() => {
+            throw new Error(NAN_ERROR)
+        })
+
+        expect(() => div(NaN, NaN)).toThrowError(NAN_ERROR)
+    })
+
+    it('should return 0 if we divide 0 by any number', () => {
+        expect(div(0, 2)).toBe(0)
+    })
+
+    it('should return Infinity if we divide any number by 0', () => {
+        expect(div(2, 0)).toBe(Infinity)
+    })
+
+    it('should return -Infinity if we divide any number by 0', () => {
+        expect(div(-2, 0)).toBe(-Infinity)
     })
 })
