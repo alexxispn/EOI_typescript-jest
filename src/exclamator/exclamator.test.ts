c20e77c0eda1fb8814f9ae1ecafb485d00d005e1
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-09-13 17:17:45 +0100

    feat/test(exclamator): Refactor exclamator to be more scalable

--- src/exclamator/exclamator.test.ts
+++ src/exclamator/exclamator.test.ts
@@ -1,5 +1,5 @@
-import {removeExclamations} from "./exclamator";
-import {countChars} from "../helpers/countChars";
+import {exclamator} from "./exclamator"
+import {countChars} from "../helpers/countChars"
 
 jest.mock('../helpers/countChars', () => ({
     countChars: jest.fn(() => 50)
@@ -7,42 +7,42 @@ jest.mock('../helpers/countChars', () => ({
 
 describe("remove-exclamations", () => {
     it("should return an object with the 'total' number ocurrences and the 'value' string", () => {
-        expect(removeExclamations("Hello World!!")).toEqual({
+        expect(exclamator("Hello World!!")).toEqual({
             total: 50,
             value: "Hello World!!",
-        });
+        })
     })
     it("should return an object with the 'total' number ocurrences, the 'value' string and the 'limit' string if there is more than 50 occurrences", () => {
         jest.mocked(countChars).mockReturnValueOnce(51);
-        expect(removeExclamations("Hello World!!")).toEqual({
+        expect(exclamator("Hello World!!")).toEqual({
             total: 51,
             value: "Hello World!!",
             limit: "It has passed the limit of 50 exclamation marks",
-        });
+        })
     })
     it("should return an object with the 'total' number ocurrences, the 'value' string and the 'limit' string if there is more than 100 occurrences", () => {
         jest.mocked(countChars).mockReturnValueOnce(101);
-        expect(removeExclamations("Hello World!!")).toEqual({
+        expect(exclamator("Hello World!!")).toEqual({
             total: 101,
             value: "Hello World!!",
             limit: "It has passed the limit of 100 exclamation marks",
-        });
+        })
     })
     it("should return an object with the 'total' number ocurrences, the 'value' string and the 'limit' string if there is more than 150 occurrences", () => {
         jest.mocked(countChars).mockReturnValueOnce(151);
-        expect(removeExclamations("Hello World!!")).toEqual({
+        expect(exclamator("Hello World!!")).toEqual({
             total: 151,
             value: "Hello World!!",
             limit: "It has passed the limit of 150 exclamation marks",
-        });
+        })
     })
     it("should return an object with the 'total' number ocurrences, the 'value' string and the 'limit' string if there is more than 200 occurrences", () => {
         jest.mocked(countChars).mockReturnValueOnce(201);
-        expect(removeExclamations("Hello World!!")).toEqual({
+        expect(exclamator("Hello World!!")).toEqual({
             total: 201,
             value: "Hello World!!",
             limit: "It has passed the last limit of 200 exclamation marks",
             invalid: true,
-        });
+        })
     })
 })
--- src/exclamator/exclamator.ts
+++ src/exclamator/exclamator.ts
@@ -11,24 +11,24 @@ import {countChars} from "../helpers/countChars"
 
 // if there is more than 200 occurrences we should add new prop 'limit' to object with 'It has passed the last limit' value and another prop 'invalid' with true value
 
-export const removeExclamations = (sentence: string, limit: number = 200) => {
+export const exclamator = (sentence: string, limit: number = 200) => {
     const occurrences = countChars('!', sentence);
     let result: { total: number; value: string; limit?: string; invalid?: boolean } = {
         total: occurrences,
         value: sentence,
     }
 
-    if (occurrences <= 50) return result;
+    if (occurrences <= 50) return result
 
     if (occurrences > limit) {
         result.limit = `It has passed the last limit of ${limit} exclamation marks`;
-        result.invalid = true;
+        result.invalid = true
     } else {
         for (let i = limit; i >= limit - 150; i -= 50) {
             if (occurrences > i - 50) {
                 const limitNumber = i - 50
                 result.limit = `It has passed the limit of ${i - 50} exclamation marks`;
-                break;
+                break
             }
         }
     }
