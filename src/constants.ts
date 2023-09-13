815814d46d1f738a5815ebde5848e5dd3acbc4df
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-09-13 17:18:26 +0100

    Add ERROR_CHARACTER_LENGTH to constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const ERROR_NAN = 'Error: one of the numbers is not a number'
 export const ERROR_NUMBER_IS_TOO_BIG_OR_TOO_SMALL = 'One of the numbers is too big or too small'
+export const ERROR_CHARACTER_LENGTH = 'The character to find must be a single character'
